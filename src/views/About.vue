dd0ab1192c16a760d7deb519cd2310486b2ad036
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-05-08 01:04:20 +0530

    Working on Components

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Me</h1>
+
+    <p>I am Manoj Tyagi. I am a full-stack developer. I am currently in my pre-final year of Undergraduation. I have experience in working with Django.</p>
   </div>
 </template>
