def79422b843b7f129a59806249e924aa6ee2733
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-05-08 01:13:24 +0530

    Working on Components

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,17 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 #nav {
   padding: 30px;
+  display: flex;
+  justify-content: center;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  margin: 0 20px;
 }
 
 #nav a.router-link-exact-active {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="about">
-    <h1>About Me</h1>
+    <h1 class="center teal-text">About Me</h1>
 
-    <p>I am Manoj Tyagi. I am a full-stack developer. I am currently in my pre-final year of Undergraduation. I have experience in working with Django.</p>
+    <p class="center dark-grey-text">I am Manoj Tyagi. I am a full-stack developer. I am currently in my pre-final year of Undergraduation. I have experience in working with Django.</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<style>
+.about {
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.about p {
+  max-width: 550px;
+  font-size: 1.2em
+}
+</style>
